ed916a3c191a70c96c96c87570d64da187a44958
Victor M Moreira <109500795+Pir4te64@users.noreply.github.com>
2024-05-04 13:24:22 -0300

    INDEX

---
+++ Index.js
@@ -0,0 +1,10 @@
+<html>
+    <head></head>
+    <body>
+        "Hello js rocket
+        "
+        <script type="text/javascript">
+            console.log('Hello there friends!');
+        </script>   
+    </body>
+</html>
